7aed8dba0d8a8eb6698ba01d8a84e4537a6f4241
Josh Thompson <josht200@live.com>
2023-02-15 19:30:52 +0000

    Added allow to top of app

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname)));
+app.use(function(req, res, next) { // Thank you ChatGPT
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader("Access-Control-Allow-Methods", "POST");
+   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+   next();
+ });
+ 
    
 // create reusable transporter object using default transport
    let transporter = nodemailer.createTransport({
